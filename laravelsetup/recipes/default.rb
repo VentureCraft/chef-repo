63e30d30a12560291ecc48fb70a7a310372fd47f
Chris Duell <me@chrisduell.com>
2013-04-23 16:30:17 +1000

    syntax error

--- laravelsetup/recipes/default.rb
+++ laravelsetup/recipes/default.rb
@@ -34,7 +34,7 @@ node[:deploy].each do |app_name, deploy|
 
     # workbenches = ['vcpermission', 'onboarding', 'revisionable', 'vcmail'];
     directory = '#{deploy[:deploy_to]}/current/workbench/venturecraft'
-    workbenches = Dir.entries(directory).select { |file| File.directory? File.join(directory, file}
+    workbenches = Dir.entries(directory).select { |file| File.directory? File.join(directory, file) }
 
     workbenches.each { |workbench|
         bash "workbench_composer_install_command" do
